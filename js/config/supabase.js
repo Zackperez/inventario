c2002beccec89b69297eae761d14b97e02c82e16
Zacks <53922473+Zackperez@users.noreply.github.com>
2023-11-17 22:01:18 -0500

    Add files via upload

---
+++ js/config/supabase.js
@@ -0,0 +1,8 @@
+const config = {
+    headers: {
+      apikey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlvd3RyZ3V0cGdkaG91enhudm5hIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDAyMzExMzgsImV4cCI6MjAxNTgwNzEzOH0.Dat7C5Ji_3qKsrR5oUcJeiAeV41owhKyfyzwduxpH4s',
+      Authorization: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlvd3RyZ3V0cGdkaG91enhudm5hIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDAyMzExMzgsImV4cCI6MjAxNTgwNzEzOH0.Dat7C5Ji_3qKsrR5oUcJeiAeV41owhKyfyzwduxpH4s'
+    }
+  };
+
+  export default config;
