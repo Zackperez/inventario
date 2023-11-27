b66ade0c746a776632bcb6f5688ef7d39046aba4
Zacks <53922473+Zackperez@users.noreply.github.com>
2023-11-27 10:58:57 -0500

    ruta editar usuarios y modal

--- js/personal/personal.js
+++ js/personal/personal.js
@@ -50,6 +50,23 @@ const Modelo = {
         return res;
     },
 
+    async editarUsuario(id, nombre, puesto, celular) {
+
+        const datos_insertar = {
+            nombre: nombre,
+            puesto: puesto,
+            celular: celular
+        }
+
+        const res = await axios({
+            method: "PATCH",
+            url: 'https://iowtrgutpgdhouzxnvna.supabase.co/rest/v1/personal?id=eq.' + id,
+            headers: config.headers,
+            data: datos_insertar
+        });
+        return res;
+    },
+
     async eliminarDatosTabla(ref) {
 
         const res = await axios({
@@ -63,47 +80,72 @@ const Modelo = {
 
 const Controlador = {
 
-    async traerUsuarios(){
+    async traerUsuarios() {
         const res = await Modelo.traerUsuarios();
         Vista.mostrarUsuarios(res)
     },
+
+    async editarUsuario(id, nombre, puesto, celular) {
+
+        const res = await Modelo.editarUsuario(id, nombre, puesto, celular);
+
+        console.log(res)
+
+    }
 }
 
 const Vista = {
-    
-    mostrarUsuarios(res){
+
+    mostrarUsuarios(res) {
         const data = res.data
         data.forEach(element => {
             const contenedorUsuarios = document.getElementById('contenedorUsuarios');
             const usuario = document.createElement('div')
             usuario.classList.add('usuario')
             usuario.innerHTML =
-            `
+                `
             <div class="foto-usuario">
                 <img src="../img/perfil.svg" alt="">
             </div>
 
             <div class="datos-personales">
                 <div class="nombre">
-                    <p>Jean Carlos Trujillo Pérez</p>
+                    <p>${element.nombre}</p>
                 </div>
 
                 <div class="puesto">
-                    <p>Puesto: Master en la programacion</p>
+                    <p>Puesto: ${element.puesto} </p>
                 </div>
 
                 <div class="celular">
-                    <p>Celular: 3113109661</p>
+                    <p>Celular: ${element.celular}</p>
                 </div>
 
             </div>
 
             <div class="botones">
-                <button>Editar</button>
+                <button class = "boton-editar-usuario">Editar</button>
             </div>
             `;
 
             contenedorUsuarios.append(usuario);
+
+            usuario.querySelector('.boton-editar-usuario').addEventListener('click', function () {
+                Controlador.editarUsuario(element.id, "Nayeon", "idol", "3113109662")
+                Swal.fire({
+                    title: `¿Estás seguro? `,
+                    icon: "warning",
+                    showCancelButton: true,
+                    confirmButtonColor: "#3085d6",
+                    cancelButtonColor: "#d33",
+                    confirmButtonText: "Agregar"
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        const ref = dato['ref'];
+                        Controlador.eliminarDatosFormulario(ref);
+                    }
+                });
+            })
         });
     }
 
@@ -112,3 +154,22 @@ const Vista = {
 document.addEventListener('DOMContentLoaded', function () {
     Controlador.traerUsuarios()
 })
+
+/* MODAL Agregar datos */
+var modal = document.getElementById('targetModalEditarUsuario');
+var abrirModalEditarUsuario = document.getElementById('abrirModalEditarUsuario');
+var btnCerrarModalUsuario = document.getElementsByClassName('cerrar-modal-editar-usuario')[0];
+
+abrirModalEditarUsuario.onclick = function () {
+    modal.style.display = "block";
+}
+
+btnCerrarModalUsuario.onclick = function () {
+    modal.style.display = "none";
+}
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
